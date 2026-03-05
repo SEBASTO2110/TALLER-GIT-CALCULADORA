a85ea874551cfe43f1cccc59c105819d785b1944
SEBASTO2110 <sebasto2110@gmail.com>
2026-03-04 21:36:07 -0500

    feat: correccion de errores

--- usuario_b.py
+++ usuario_b.py
@@ -1,17 +1,25 @@
 def es_primo(n):
     if n < 2:
-        return False
+        return 
     for i in range(2, n):
             if n % i == 0:
-                return False
-    return True
+                print("No es primo")
+                return 
+    print("Es primo")
 
 def primos_en_rango(inicio, fin):
     primos = []
     for num in range(inicio, fin + 1):
-        if es_primo(num):
-            primos.append(num) 
-    return primos
+        primo = True
+        if num < 2:
+            primo = False
+        for i in range(2, num):
+            if num % i == 0:
+                primo = False
+                break
+        if primo:
+            primos.append(num)
+    print("Primos en el rango:", primos)
 
 def factorial(n):
     if n < 0:
@@ -19,9 +27,9 @@ def factorial(n):
     resultado = 1
     for i in range(1, n + 1):
         resultado *= i
-    return resultado
+    print("El factorial es:", resultado)
 
 def mcd(a, b):
     while b != 0:
         a, b = b, a % b
-    return a
+    print("El MCD es: ", a)
